1e0edcd9314009c6896619b6cdd250430f83aeb9
Abhishek Solanki <abhisolanki94@gmail.com>
2019-03-10 10:17:45 +0530

    beta0.0.1 release
    error handling 404
    exceptiom handled in fs read and write

--- hbserver.js
+++ hbserver.js
@@ -23,20 +23,21 @@ app.get("/", function(req,res){
 	res.render("index",retData);
 });
 
-app.post("/persist_hyp001",function(req,res){
-	console.log("body: "+JSON.stringify(req.body));
+app.put("/persist_hyp001",function(req,res){
+	//console.log("body: "+JSON.stringify(req.body));
 	// if there is no permalink then it is new document and requires id
 	if (typeof req.body.permalink === "undefined" || req.body.permalink === "" || req.body.permalink === null){
 		urlId = shortid.generate(); 
+		console.log("new permalink: "+urlId);
 	} else {
 		urlId = req.body.permalink
+		console.log("modified request for permalink: "+urlId);
 	}
-	console.log("permalink : "+urlId);
 	req.body.permalink = urlId;
 	json_obj = JSON.stringify(req.body);
 	fs.writeFile(__dirname+"/data/"+urlId+".json",json_obj,"utf-8",function(err){
 		if(err) throw err;
-		console.log("document "+urlId+" saved!");
+		console.log("document persisted: "+urlId);
 		retObj = {};
 		retObj.permalink = urlId;
 		retObj.createNew = false;
@@ -44,23 +45,52 @@ app.post("/persist_hyp001",function(req,res){
 	});
 });
 
+app.delete("/delete_hyp001",function(req,res){
+	//console.log("body: "+JSON.stringify(req.body));
+	urlId = req.body.permalink
+	console.log("delete request for: "+urlId);
+	json_obj = JSON.stringify(req.body);
+	try{
+		fs.unlink(__dirname+"/data/"+urlId+".json",function(err){
+			if(err) console.log(err);
+		});
+		console.log("deleted permalink: "+urlId);
+		retObj = {};
+		retObj.permalink = urlId;
+		retObj.createNew = false;
+		res.send(retObj);
+	}catch(err){
+		console.log("file not found: "+urlId);
+		res.render("404");
+	}
+	
+});
+
 app.get('/doc*',function(req,res){
 	var urlObj = url.parse(req.url,true);
 	docId = urlObj.path.split('__')[1];
-	let rawData = fs.readFileSync(__dirname+"/data/"+docId+".json","utf-8",function(err){
-		if(err) throw err;
-	});
-	let retData = JSON.parse(rawData);
-	retData.createNew = false;
-	//console.log(JSON.stringify(retData))
-	res.render("index",retData);
+	console.log("visited permalink: "+docId);
+	var rawData;
+	try {
+		rawData = fs.readFileSync(__dirname+"/data/"+docId+".json","utf-8");
+		let retData = JSON.parse(rawData);
+		retData.createNew = false;
+		//console.log(JSON.stringify(retData))
+		res.render("index",retData);
+	} catch(err) {
+		if(err.code === 'ENOENT'){
+			console.log("file not found: "+docId);
+		} 
+		res.render("404");
+	}
+	
 });
 
 app.get("/faq", function(req,res){
 	res.render("faq");
 });
 
-var server = app.listen(8080,function(){
+var server = app.listen(80,function(){
 	var host = server.address().address
 	var port = server.address().port
 	console.log("app listening at http://%s:%s",host,port)
